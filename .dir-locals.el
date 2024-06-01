2d5d8c7904921fb217eb8be722c616aee9d7ea26
Aryadev Chavali <aryadev@aryadevchavali.com>
2024-06-01 01:18:56 +0100

    Update AVM and clean dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,10 +1,9 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil    . ((projectile-project-compilation-cmd . "make")
-            (projectile-project-configure-cmd   . "make clean")
-            (+license/license-choice            . "GPLv2")))
- (c++-mode . ((flycheck-gcc-include-path          . (".." "../avm"))
-              (flycheck-clang-include-path        . (".." "../avm"))
-              (company-clang-arguments            . ("-I.." "-I../avm"))
-              (eval                               . (clang-format-mode t)))))
+((nil      . ((+license/license-choice     . "GPLv2")))
+ (c++-mode . ((flycheck-gcc-include-path   . (".." "../avm"))
+              (flycheck-clang-include-path . (".." "../avm"))
+              (company-clang-arguments     . ("-I.." "-I../avm"))
+              (mode                        . clang-format)
+              (eval                        . (eglot-ensure)))))
