d4d2b4715d75ce14ee84a350786f0be54b04bf3c
Aryadev Chavali <aryadev@aryadevchavali.com>
2024-07-07 19:08:31 +0100

    Update gitignore and dir locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,8 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil      . ((+license/license-choice     . "GPLv2")))
+((nil      . ((+license/license-choice     . "GNU General Public License Version 2")
+              (compile-command             . "make all VERBOSE=2 RELEASE=0")))
  (c++-mode . ((flycheck-gcc-include-path   . (".." "../avm"))
               (flycheck-clang-include-path . (".." "../avm"))
               (company-clang-arguments     . ("-I.." "-I../avm"))
